105e4aa57bf88a613112c0e9f537c3d6579471d1
mehran13mome <mehran13mome@gmail.com>
2022-01-12 21:02:29 +0330

    Amend register to authentication

--- routes/administrators/register/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { validate } = require('express-validation');
-const router = express.Router();
-
-const { registerValidation } = require('../../validators/registration/index');
-
-router.post('/', validate(registerValidation('register')), createAdministrator);
-
-module.exports = router;
