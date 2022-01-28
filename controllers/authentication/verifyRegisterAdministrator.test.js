1818fa16540e28c316ef3a478a1bb7840cb5b3e8
mehran13mome <mehran13mome@gmail.com>
2022-01-28 12:04:47 +0330

    Clear verifyRegister test

--- controllers/authentication/verifyRegisterAdministrator.test.js
+++ controllers/authentication/verifyRegisterAdministrator.test.js
@@ -1,9 +1,10 @@
 const { chai, server } = require('../../test/testConfig');
 const TempAdministrators = require('../../models/administrators/TempAdministrators');
+const Administrators = require('../../models/administrators/Administrators');
 
-describe('Register', () => {
+describe('Register Verification', () => {
 	const testData = {
-		email: 'mehran@test.com',
+		email: 'mehran13Verify@test.com',
 		fullName: 'mehrani',
 		OTP: 1234,
 	};
@@ -27,10 +28,17 @@ describe('Register', () => {
 				.post('/api/auth/verify-register')
 				.send({ email: testData.email, OTP: testData.OTP })
 				.end((err, res) => {
+					if (err) done(err);
 					res.should.have.status(200);
-					// res.should.have.property('message').eql('Admin verified');
 					done();
 				});
+		});
+
+		after((done) => {
+			Administrators.deleteOne({ email: testData.email }, null, (err) => {
+				if (err) done(err);
+				done();
+			});
 		});
 	});
 });
